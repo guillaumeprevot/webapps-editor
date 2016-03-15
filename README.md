aa0be72a5d0347043deac66758c3ff8ba464cc7a
guillaumeprevot <guillaume.prevot@gmx.fr>
2016-03-15 22:50:27 +0100

    Première version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # webapps-editor
+
 A simple HTML editor using `document.execCommand` capabilities
+https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand
+
+## Présentation
+
+[Cette application](http://techgp.fr/webapps/webapps-editor.html) écrite en HTML5, JavaScript et CSS3 est un éditeur de texte [WISIWYG](https://fr.wikipedia.org/wiki/What_you_see_is_what_you_get) pour l'HTML.
+
+Les librairies suivantes ont été utilisées pour cette application :
+
+- [jQuery 2.2.1](http://jquery.com/) sous licence MIT
+- [Bootstrap 3.3.6](http://getbootstrap.com/css/) sous licence MIT
+- [Bootstrap Colorpicker 2.3.0](https://github.com/mjolnic/bootstrap-colorpicker/) sous licence Apache 2.0
+- [Font Awesome 4.5.0](http://fortawesome.github.io/Font-Awesome/) SIL OFL 1.1 (police) et MIT (code)
+
+L'application est fournie avec un fichier manifest `webapps-editor.appcache` permettant la mise en cache et l'utilisation en mode déconnecté. Plus d'info chez Mozilla [en français](https://developer.mozilla.org/fr/docs/Utiliser_Application_Cache) ou [en anglais](https://developer.mozilla.org/en-US/docs/Web/HTML/Using_the_application_cache).
+
+NB : quand le certificat HTTPS est incorrect, la mise en cache échouera sous Chrome avec l'erreur `Manifest fetch Failed (9)`. Dans ce cas, faites les tests en HTTP et/ou utilisez un certificat valide en production.
+
+## Captures d'écran
+
+### Présentation de l'IHM
+
+![Présentation de l'IHM](./screenshots/webapps-editor-1.png)
+
---
+++ webapp/webapps-editor.js
@@ -0,0 +1,179 @@
+// document.designMode = "on";
+
+$(function() {
+	document.execCommand('styleWithCSS', null, true); // true par défaut
+
+	// Désactiver les boutons non supportés
+	$('#editor-toolbar button[data-command]').each(function(index, element) {
+		var self = $(element);
+		if (!document.queryCommandSupported(self.attr('data-command'))) {
+			self.prop('disabled', true).attr('title', self.attr('title') + ' [Non supporté par votre navigateur]');
+		}
+	});
+
+	// Gérer automatiquement le clic sur les boutons simples
+	$('#editor-toolbar').on('click', 'button.command', function(event) {
+		document.execCommand($(event.target).attr('data-command'));
+	});
+
+	// Gérer automatiquement le clic sur les boutons "block-command"
+	$('#editor-toolbar').on('click', 'button.block-command', function(event) {
+		var self = $(event.target);
+		document.execCommand(self.attr('data-command'), null, self.attr('data-tag') || 'p');
+	});
+
+	// Gérer automatiquement le clic sur les boutons "prompt-command"
+	$('#editor-toolbar').on('click', 'button.prompt-command', function(event) {
+		var self = $(event.target),
+			value = window.prompt(self.attr('data-prompt'), self.attr('data-default'));
+		if (value)
+			document.execCommand(self.attr('data-command'), null, (self.attr('data-format') || '%VALUE%').replace('%VALUE%', value));
+	});
+
+	// Modification de la propriété "font-family"
+	var fontFamilyMenu = $('#font-family-menu');
+	fontFamilyMenu.on('click', 'button:first-child', function(event) {
+		// Clic sur le bouton indiquant la dernière police utilisée
+		var self = $(event.target),
+			fontFamily = self.attr('data-font-family');
+		document.execCommand('fontName', null, fontFamily);
+	}).on('click', 'ul > li:not(:last-child) > a', function(event) {
+		// Clic sur une police dans le menu déroulant
+		var self = $(event.target).closest('a'),
+			fontFamily = self.attr('data-font-family');
+		document.execCommand('fontName', null, fontFamily);
+		// Cette police passe sur le bouton principal, comme dernière police utilisée
+		fontFamilyMenu.children('button:first-child').attr('data-font-family', fontFamily).text(fontFamily);
+		// Eviter le # dans l'URL
+		event.preventDefault();
+	}).on('click', 'ul > li:last-child > a', function(event) {
+		// Clic sur l'entrée "..." qui permet de choisir une police au choix
+		var fontFamily = prompt('Famille', '');
+		if (fontFamily) {
+			document.execCommand('fontName', null, fontFamily);
+			// Cette police passe sur le bouton principal, comme dernière police utilisée
+			fontFamilyMenu.children('button:first-child').attr('data-font-family', fontFamily).text(fontFamily);
+		}
+		// Eviter le # dans l'URL
+		event.preventDefault();
+	});
+
+	// Modification de la propriété "font-size"
+	var fontSizeMenu = $('#font-size-menu');
+	fontSizeMenu.on('click', 'button:first-child', function(event) {
+		// Clic sur le bouton indiquant la dernière taille utilisée
+		var self = $(event.target),
+			fontSize = self.attr('data-font-size');
+		document.execCommand('fontSize', null, fontSize);
+	}).on('click', 'ul > li > a', function(event) {
+		// Clic sur une taille dans le menu déroulant
+		var self = $(event.target).closest('a'),
+			fontSize = self.attr('data-font-size');
+		document.execCommand('fontSize', null, fontSize);
+		// Cette taille passe sur le bouton principal, comme dernière taille utilisée
+		fontSizeMenu.children('button:first-child').attr('data-font-size', fontSize).text(fontSize);
+		// Eviter le # dans l'URL
+		event.preventDefault();
+	});
+
+	// Modification de la couleur du texte
+	var colorMenu = $('#color-menu');
+	colorMenu.on('click', 'button:first-child', function(event) {
+		// Clic sur le bouton indiquant la dernière couleur utilisée
+		var self = $(event.target),
+			color = self.css('color');
+		document.execCommand('foreColor', null, color);
+	}).on('click', 'ul > li:not(:last-child) > a', function(event) {
+		// Clic sur une couleur dans le menu déroulant
+		var self = $(event.target),
+			color = self.css('color');
+		document.execCommand('foreColor', null, color);
+		// Cette couleur passe sur le bouton principal, comme dernière couleur utilisée
+		colorMenu.children('button:first-child').css('color', color);
+		// Eviter le # dans l'URL
+		event.preventDefault();
+	});
+	// Rechercher l'entrée "..." dans le sous-menu
+	colorMenu.find('ul > li:last-child > a').colorpicker({
+		color: '#000000'
+	}).on('changeColor', function(event) {
+		// Choix d'une couleur personnalisée
+		var color = event.color.toHex();
+		document.execCommand('foreColor', null, color);
+	}).on('hidePicker', function(event) {
+		var color = event.color.toHex();
+		// Cette couleur passe sur le bouton principal, comme dernière couleur utilisée
+		colorMenu.children('button:first-child').css('color', color);
+		// Ajouter une entrée de menu pour la retrouver facilement
+		$('<li><a href="#" style="color: ' + color + ';">Abc def</a></li>').prependTo(colorMenu.children('ul'));
+	});
+
+	// Modification de la couleur de fond
+	var backgroundColorMenu = $('#background-color-menu');
+	backgroundColorMenu.on('click', 'button:first-child', function(event) {
+		// Clic sur le bouton indiquant la dernière couleur utilisée
+		var self = $(event.target).children(),
+			color = self.css('background-color');
+		document.execCommand('backColor', null, color);
+	}).on('click', 'ul > li:not(:last-child) > a', function(event) {
+		// Clic sur une couleur dans le menu déroulant
+		var self = $(event.target),
+			color = self.css('background-color');
+		document.execCommand('backColor', null, color);
+		// Cette couleur passe sur le bouton principal, comme dernière couleur utilisée
+		backgroundColorMenu.children('button:first-child').children().css('background-color', color);
+		// Eviter le # dans l'URL
+		event.preventDefault();
+	});
+	// Rechercher l'entrée "..." dans le sous-menu
+	backgroundColorMenu.find('ul > li:last-child > a').colorpicker({
+		color: '#FFFFFF'
+	}).on('changeColor', function(event) {
+		// Choix d'une couleur personnalisée
+		var color = event.color.toHex();
+		document.execCommand('backColor', null, color);
+	}).on('hidePicker', function(event) {
+		var color = event.color.toHex();
+		// Cette couleur passe sur le bouton principal, comme dernière couleur utilisée
+		backgroundColorMenu.children('button:first-child').children().css('background-color', color);
+		// Ajouter une entrée de menu pour la retrouver facilement
+		$('<li><a href="#" style="background-color: ' + color + ';">&nbsp;</a></li>').prependTo(backgroundColorMenu.children('ul'));
+	});
+
+	/**
+	 * Raccourcis
+	 * - Ctrl + O : Ouvrir (Open)
+	 * - Ctrl + S : Enregistrer (Save)
+	 * - Ctrl + B : Gras (Bold)
+	 * - Ctrl + I : Italic (Italic)
+	 * - Ctrl + U : Souligné (Underline)
+	 * - Ctrl + Maj + B : Indice (Base)
+	 * - Ctrl + Maj + P : Exposant (Power) 
+	 * - Ctrl + M : Retirer le formatage
+	 * - Ctrl + L : Aligné à gauche (Left)
+	 * - Ctrl + E : Centré (??)
+	 * - Ctrl + R : Aligné à droite (Right)
+	 * - Ctrl + J : Justifié (Justified)
+	 * - Ctrl + K : Créer un lien
+	 * - Ctrl + X : Couper (Cut)
+	 * - Ctrl + C : Copier (Copy)
+	 * - Ctrl + V : Coller (Paste)
+	 * - Ctrl + F12 : Insérer un tableau
+	 * - Ctrl + XXX : 
+	 * - Ctrl + XXX : 
+	 * - F12 : Liste numérotée
+	 * - Maj + F12 : Liste à puces
+	 * Autres commandes dispo
+	 * - enableInlineTableEditing <bool>
+	 * - enableObjectResizing <bool>
+	 * - formatBlock <tag>
+	 * - forwardDelete
+	 * - heading <h1..h6>
+	 * - hiliteColor <color>
+	 * - insertBrOnReturn <bool>
+	 * - insertParagraph
+	 * - insertText <text>
+	 * - selectAll
+	 */
+
+});
